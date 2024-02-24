03bc540979845ccc164354532e8045383261175a
ss987 <98395295+ss987@users.noreply.github.com>
2024-02-24 17:24:06 +0530

    changesv2

--- allyfind/components/auth.jsx
+++ allyfind/components/auth.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import {createUserWithEmailAndPassword,signOut} from "firebase/auth"
 import { auth } from "../src/config/firebase"
 import { collection, addDoc } from "firebase/firestore";
-import { db } from './config/firebase';
+import { db } from '../src/config/firebase';
 export const Auth=()=>{
     const userref=collection(db,"user")
     const detailsref=collection(db,"details")
