ad783ddfa89ac63a62c942522605a4e18d4b5157
ss987 <98395295+ss987@users.noreply.github.com>
2024-02-24 14:56:46 +0530

    trial

--- allyfind/src/App.jsx
+++ allyfind/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+
 import './App.css'
 import {Auth} from '../components/auth'
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     
       <div>
+      <Auth/>
       </div>
   )
 }
